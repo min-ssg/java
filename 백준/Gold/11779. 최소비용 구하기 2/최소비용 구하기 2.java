ac6663df997290635da713f9a2acb8301751e8ff
김민석(Minseok Kim) <49543756+minsssg@users.noreply.github.com>
2022-08-15 22:49:01 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 588 ms, Memory: 54728 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -2,26 +2,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             FastReader scan = new FastReader(br);
-            int N = scan.nextInt(); // 도시의 개수
-            int M = scan.nextInt();
 
-            int[][] inputs = new int[M][3]; // start end weight;
-            for (int i = 0; i < M; i++) {
+            int numberOfCity = scan.nextInt();
+            int numberOfBus = scan.nextInt();
+
+            int[][] busInfo = new int[numberOfBus][3];
+            for (int i = 0; i < numberOfBus; i++) {
                 for (int j = 0; j < 3; j++) {
-                    inputs[i][j] = scan.nextInt();
+                    busInfo[i][j] = scan.nextInt();
                 }
             }
+
             int start = scan.nextInt();
             int end = scan.nextInt();
 
-            Solution1916 solution = new Solution1916();
-            solution.solve(N,M,inputs,start,end);
+            Solution11779 solution = new Solution11779();
+            solution.solve(numberOfCity, numberOfBus, busInfo, start, end);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -48,78 +51,92 @@ public class Main {
     }
 }
 
-class Solution1916 {
+class Solution11779 {
 
-    private int[] COST;
+    StringBuilder sb = new StringBuilder();
 
-    public void solve(int n, int m, int[][] inputs, int start, int end) {
+    List<Edge>[] graph;
+    int[] COST;
+    int[] routes;
 
-        List<Point>[] graph = new ArrayList[n + 1];
-        int[] routes = new int[n + 1];
-
-        for (int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
-        for (int[] input: inputs) {
-            int s = input[0];
-            int e = input[1];
-            int w = input[2];
-
-            graph[s].add(new Point(e, w));
-        }
-        StringBuilder sb = new StringBuilder();
-
-        this.COST = new int[n + 1];
+    public void solve(int numberOfCity, int numberOfBus, int[][] busInfo, int start, int end) {
+        initGraph(numberOfCity, busInfo);
+        this.COST = new int[numberOfCity + 1];
+        this.routes = new int[numberOfCity + 1];
         Arrays.fill(COST, Integer.MAX_VALUE);
-        COST[start] = 0;
-        PriorityQueue<Point> pq = new PriorityQueue<>();
-        pq.add(new Point(start, 0));
 
-        while (!pq.isEmpty()) {
-            Point point = pq.poll();
-            int target = point.end;
-            int cost = point.cost;
-            // 최단거리가 아닌 경우 PASS
-            if (COST[target] < cost) continue;
-            for (Point adjacent : graph[target]) {
-                int next = adjacent.end;
-                int nextCost = cost + adjacent.cost;
-                if (nextCost < COST[next]) {
-                    COST[next] = nextCost;
-                    routes[next] = target;
-                    pq.add(new Point(next, nextCost));
-                }
-            }
-        }
+        dijkstra(start, end);
+        List<Integer> path = getPath(start, end);
+        sb.append(COST[end]).append('\n');
+        sb.append(path.size()).append('\n');
+        sb.append(path.stream().map(String::valueOf).collect(Collectors.joining(" ")));
+        System.out.println(sb.toString());
+    }
 
+    private List<Integer> getPath(int start, int end) {
         List<Integer> path = new ArrayList<>();
-        int order = end;
-        path.add(order);
-        while (order != start) {
-            path.add(routes[order]);
-            order = routes[order];
+        int city = end;
+        path.add(end);
+        while (city != start) {
+            path.add(routes[city]);
+            city = routes[city];
         }
 
         Collections.reverse(path);
-        String collect = path.stream().map(String::valueOf).collect(Collectors.joining(" "));
-        System.out.println(COST[end]);
-        System.out.println(path.size());
-        System.out.println(collect);
-
+        return path;
     }
 
-    private class Point implements Comparable<Point> {
-        int end;
+    private void dijkstra(int start, int end) {
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.add(new Edge(start, 0));
+        COST[start] = 0;
+
+        while (!pq.isEmpty()) {
+            Edge edge = pq.poll();
+            int city = edge.city;
+            int cost = edge.cost;
+
+            if (COST[city] < cost) continue; // 현재 위치에서 city까지의 거리가 최단거리가 아닌 경우, 계산할 의미가 없음.
+            for (Edge nextEdge : graph[city]) {
+                int nextCity = nextEdge.city;
+                int nextCost = nextEdge.cost;
+
+                if (COST[nextCity] > cost + nextCost) {
+                    COST[nextCity] = cost + nextCost;
+                    routes[nextCity] = city; // 다음 city로 가기 위해 현재 city를 거쳐간다는 의미.
+                    pq.add(new Edge(nextCity, cost + nextCost));
+                }
+            }
+        }
+    }
+
+    private void initGraph(int numberOfCity, int[][] busInfo) {
+        this.graph = new ArrayList[numberOfCity + 1];
+        for (int i = 1; i <= numberOfCity; i++) this.graph[i] = new ArrayList<>();
+
+        for (int[] info : busInfo) {
+            int s = info[0];
+            int e = info[1];
+            int cost = info[2];
+
+            graph[s].add(new Edge(e, cost));
+        }
+    }
+
+    /**
+     * 현재 위치에서 city까지의 cost를 담는 structor class
+     */
+    private class Edge implements Comparable<Edge> {
+        int city;
         int cost;
 
-        Point(int end, int cost) {
-            this.end = end;
+        public Edge(int city, int cost) {
+            this.city = city;
             this.cost = cost;
         }
 
         @Override
-        public int compareTo(Point other) {
+        public int compareTo(Edge other) {
             return Integer.compare(this.cost, other.cost);
         }
     }

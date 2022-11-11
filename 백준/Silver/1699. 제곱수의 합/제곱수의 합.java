fb9a2fab71c89cb8778e9577e9a130f69e8ffdf9
김민석(Minseok Kim) <49543756+minsssg@users.noreply.github.com>
2022-11-11 14:11:37 +0900

    [Silver II] Title: 제곱수의 합, Time: 168 ms, Memory: 14616 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            FastReader scan = new FastReader(br);
+
+            int N = scan.nextInt();
+
+            Solution1699 solution = new Solution1699();
+            int answer = solution.solve(N);
+            System.out.println(answer);
+
+        }  catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class FastReader {
+        private final BufferedReader br;
+        private StringTokenizer st;
+
+        public FastReader (BufferedReader br) {
+            this.br = br;
+        }
+
+        public String nextLine() throws IOException {
+            return br.readLine();
+        }
+
+        public String next() throws IOException {
+            while (st == null || !st.hasMoreElements()) {
+                st = new StringTokenizer(nextLine());
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        public long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+    }
+}
+
+class Solution1699 {
+
+    private int[] dp;
+    public int solve(int n) {
+        this.dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i;
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j*j] + 1);
+            }
+        }
+        return dp[n];
+    }
+}
